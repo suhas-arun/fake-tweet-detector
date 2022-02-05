ce0a2ca81782fd3f77a1be11252ef8d9ec972368
Jacob <jjlarcombe007@outlook.com>
2022-02-05 16:09:35 +0000

    Got benfords law testing working

--- src/process_data.py
+++ src/process_data.py
@@ -13,30 +13,32 @@ bl = benfordslaw(alpha=0.05, method='ks')
 with open('src/testData.txt') as raw:
     D = raw.read()
 
-X = D.split('\n')
+X = D.split(',\n')
 
 while (X[-1] == ''):
     del X[-1]
 
 
 for index in range(0, len(X)):
-    X[index] = int(X[index]) """
+    X[index] = int(X[index])
+data = pd.array(X) """
 
 """ # Load elections example
 df = bl.import_example(data='USA')
 
 # Extract election information.
-X = df['votes'].loc[df['candidate']=='Donald Trump'].values
- """
+data = df['votes'].loc[df['candidate']=='Donald Trump'].values """
 
-X = pd.read_csv('src/testData.txt')
 
+#data = pd.read_csv('src/testData.txt',header=0)
+X = pd.DataFrame({'followers':[1,1,1,1,1,1,1,11,1,1,1, 2,2,2,2,2,2,2,2222,2,22,2,2,2,22,2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 8,9]})
+data = X['followers']
 # Print
-print(X)
+print(data)
 # array([ 5387, 23618,  1710, ...,    16,    21,     0], dtype=int64)
 
 # Make fit
-results = bl.fit(X)
+results = bl.fit(data)
 print(results)
 # Plot
 bl.plot(title='Donald Trump')
