6e6727d87ef322b2db614762c9fa5000d8a75aa2
sa2021 <sa2021@ic.ac.uk>
2022-02-05 20:43:17 +0000

    Working on getting account info

--- src/get_data.py
+++ src/get_data.py
@@ -1,4 +1,7 @@
 import snscrape.modules.twitter as sntwitter
+from itertools import islice
+from account_info import AccountInfo
+from time import time
 
 
 def get_user_id(username):
@@ -8,4 +11,36 @@ def get_user_id(username):
         return -1
 
 
-print(get_user_id("barackobama"))
+def get_tweets(username, max_tweets=100):
+    return islice(
+        sntwitter.TwitterSearchScraper(f"from:{username}").get_items(), max_tweets
+    )
+
+
+def get_account_info(username, max_tweets=100):
+    tweets = get_tweets(username, max_tweets)
+    account_id = get_user_id(username)
+    if id == -1:
+        return None
+
+    likes = []
+    retweets = []
+
+    start = time()
+    likes, retweets = get_likes_retweets(tweets)
+    t = time() - start
+
+    num_followers = sntwitter.TwitterUserScraper(username)._get_entity().followersCount
+
+    return AccountInfo(account_id, username, num_followers, likes, retweets)
+
+
+def get_likes_retweets(tweets, likes=[], retweets=[]):
+    for tweet in tweets:
+        likes.append(tweet.likeCount)
+        retweets.append(tweet.retweetCount)
+
+    return likes, retweets
+
+
+print(get_account_info("barackobama", 10))
