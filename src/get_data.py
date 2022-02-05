654f045e33d30e4c28505d08aa48063de1129149
sa2021 <sa2021@ic.ac.uk>
2022-02-05 16:41:12 +0000

    Added get tweets function

--- src/get_data.py
+++ src/get_data.py
@@ -3,12 +3,25 @@ from config import *
 
 client = tweepy.Client(bearer_token)
 
-tweets = client.search_recent_tweets(
-    query="covid", tweet_fields=["context_annotations", "created_at"], max_results=10
-)
 
-for tweet in tweets.data:
-    print("\n\n")
-    print(tweet.text)
-    if len(tweet.context_annotations) > 0:
-        print(tweet.context_annotations)
+def get_user_id(username):
+    """Gets user id for a given username. Returns -1 if user not found"""
+    try:
+        return client.get_user(username=username).data.id
+    except tweepy.HTTPException:
+        return -1
+
+
+def get_tweets(username, max_results):
+    """Gets the max_results most recent tweets from a given user"""
+    return client.search_recent_tweets(
+        query=f"from:{get_user_id(username)}",
+        tweet_fields=["public_metrics"],
+        max_results=max_results,
+    )
+
+
+tweets = get_tweets("barackobama", 10)
+
+for tweet in tweets.data or []:
+    print(f"{tweet.public_metrics}\n")
