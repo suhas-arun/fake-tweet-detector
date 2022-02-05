d2a001cfdab1792e8970b40188481907bb81f026
Jacob <jjlarcombe007@outlook.com>
2022-02-05 17:12:43 +0000

    benfords law test prcoedure done

--- src/process_data.py
+++ src/process_data.py
@@ -1,44 +1,63 @@
 # this is a test
 #This is a 2nd test
 
-
+L = 0.5
 
 from benfordslaw import benfordslaw
 import pandas as pd
 
-# Initialize
-bl = benfordslaw(alpha=0.05, method='ks')
-
-""" # get test data
-with open('src/testData.txt') as raw:
-    D = raw.read()
-
-X = D.split(',\n')
-
-while (X[-1] == ''):
-    del X[-1]
 
 
-for index in range(0, len(X)):
-    X[index] = int(X[index])
-data = pd.array(X) """
+def benfordsLawTest(data, method='ks'):
+    # conducts benfords law test on 1D array of data supplied as data
+    # mthod specifies benfords law method test
+    # returns the p value of the test
 
-""" # Load elections example
-df = bl.import_example(data='USA')
+    # Initialize
+    bl = benfordslaw(alpha=0.05, method=method)
+    # data expected as a 1D list of values
+    X = pd.DataFrame({'values':data})
+    testData = X['values']
+    results = bl.fit(testData)
+    return results['P']
 
-# Extract election information.
-data = df['votes'].loc[df['candidate']=='Donald Trump'].values """
+
+def calculateMagnitude(inputList):
+    minVal = min(inputList)
+    minOrder = len(str(minVal))-1
+    maxVal = max(inputList)
+    maxOrder = len(str(minVal))-1
+    return (maxOrder - minOrder)
+           
+
+def calulateWeightings(l, magnitudeOrder):
+    return (magnitudeOrder**l)/(6**l)
 
 
-#data = pd.read_csv('src/testData.txt',header=0)
-X = pd.DataFrame({'followers':[1,1,1,1,1,1,1,11,1,1,1, 2,2,2,2,2,2,2,2222,2,22,2,2,2,22,2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 8,9]})
-data = X['followers']
-# Print
-print(data)
-# array([ 5387, 23618,  1710, ...,    16,    21,     0], dtype=int64)
+def testProcedure(importedData, method, l):
+    # imported data will have three sets of data - likes, follows and tweets
+    # the p values of each data set will be weighted averaged
+    followers = importedData['followers']
+    likes = importedData['likes']
+    retweets = importedData['retweets']
 
-# Make fit
-results = bl.fit(data)
-print(results)
-# Plot
-bl.plot(title='Donald Trump')
+    pFollowers = benfordsLawTest(followers, method=method)
+    pLikes = benfordsLawTest(likes, method=method)
+    pRetweets = benfordsLawTest(retweets, method=method)
+    wFollowers = calulateWeightings(L, calculateMagnitude(followers))
+    wLikes = calulateWeightings(L, calculateMagnitude(likes))
+    wRetweets = calulateWeightings(L, calculateMagnitude(retweets))
+
+    pAverage = ((pFollowers * wFollowers) + (pLikes * wLikes) + (pRetweets * wRetweets))/(wFollowers + wLikes + wRetweets)
+    print("P Average:\t", pAverage)
+    return pAverage
+
+    
+
+    
+
+
+data = []
+testProcedure(data, 'ks', L)
+
+
