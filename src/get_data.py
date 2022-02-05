ed72c362e7e5cf2993e16fb3455f3218dc22dacb
William West <ww1321@ic.ac.uk>
2022-02-05 16:58:09 +0000

    Added function to get info about an account

--- src/get_data.py
+++ src/get_data.py
@@ -1,5 +1,6 @@
 import tweepy
-from config import *
+from config import bearer_token
+from account_info import AccountInfo
 
 client = tweepy.Client(bearer_token)
 
@@ -21,7 +22,17 @@ def get_tweets(username, max_results):
     )
 
 
-tweets = get_tweets("barackobama", 10)
+def get_account_info(name):
+    tweets = get_tweets(name, 10)
+    num_followers = client.get_user(username=name, user_fields=["public_metrics"]).data.public_metrics['followers_count']
+    likes = []
+    retweets = []
 
-for tweet in tweets.data or []:
-    print(f"{tweet.public_metrics}\n")
+    for tweet in tweets.data or []:
+        metrics = tweet.public_metrics
+        likes.append(metrics['like_count'])
+        retweets.append(metrics['retweet_count'])
+
+    return AccountInfo(name, num_followers, likes, retweets)
+
+print(get_account_info("barackobama"))
