0113a3f405924e5be73f3e80c687252808581e0d
sa2021 <sa2021@ic.ac.uk>
2022-02-05 20:22:03 +0000

    Switched to snscraper

--- src/get_data.py
+++ src/get_data.py
@@ -1,38 +1,11 @@
-import tweepy
-from config import bearer_token
-from account_info import AccountInfo
-
-client = tweepy.Client(bearer_token)
+import snscrape.modules.twitter as sntwitter
 
 
 def get_user_id(username):
-    """Gets user id for a given username. Returns -1 if user not found"""
     try:
-        return client.get_user(username=username).data.id
-    except tweepy.HTTPException:
+        return sntwitter.TwitterUserScraper(username)._get_entity().id
+    except (AttributeError, ValueError):
         return -1
 
 
-def get_tweets(username, max_results):
-    """Gets the max_results most recent tweets from a given user"""
-    return client.search_recent_tweets(
-        query=f"from:{get_user_id(username)}",
-        tweet_fields=["public_metrics"],
-        max_results=max_results,
-    )
-
-
-def get_account_info(name):
-    tweets = get_tweets(name, 10)
-    num_followers = client.get_user(username=name, user_fields=["public_metrics"]).data.public_metrics['followers_count']
-    likes = []
-    retweets = []
-
-    for tweet in tweets.data or []:
-        metrics = tweet.public_metrics
-        likes.append(metrics['like_count'])
-        retweets.append(metrics['retweet_count'])
-
-    return AccountInfo(name, num_followers, likes, retweets)
-
-print(get_account_info("barackobama"))
+print(get_user_id("barackobama"))
