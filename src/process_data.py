96ebbda940738076a65e18a1278c394fd2e682b2
Jacob <jjlarcombe007@outlook.com>
2022-02-05 15:05:32 +0000

    benfords law test

--- src/process_data.py
+++ src/process_data.py
@@ -1,2 +1,24 @@
 # this is a test
 #This is a 2nd test
+
+
+
+from benfordslaw import benfordslaw
+
+# Initialize
+bl = benfordslaw(alpha=0.05)
+
+# get test data
+X = []
+with open('testData.txt') as raw:
+    X.append(raw.readline)
+
+# Print
+print(X)
+# array([ 5387, 23618,  1710, ...,    16,    21,     0], dtype=int64)
+
+# Make fit
+results = bl.fit(X)
+print(results)
+# Plot
+bl.plot(title='Donald Trump')
