7c156be0109093f5ca6209851d0207faa03b4b35
Jacob <jjlarcombe007@outlook.com>
2022-02-06 00:18:29 +0000

    final benfords detection

--- src/process_data.py
+++ src/process_data.py
@@ -3,11 +3,6 @@
 
 L = 0.2
 
-from cgi import test
-from optparse import Values
-from benfordslaw import benfordslaw
-import pandas as pd
-import json
 from get_data import get_account_info
 from scipy import stats
 
@@ -21,6 +16,8 @@ numbers = {1: 0,
            7: 0,
            8: 0,
            9: 0}
+
+expected = [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
 
 def frequencyCalc(dataSet):
     for val in dataSet:
@@ -39,22 +36,6 @@ def frequencyCalc(dataSet):
     return observedFrequencies
 
 
-
-def benfordsLawTest(data, method='ks'):
-    # conducts benfords law test on 1D array of data supplied as data
-    # mthod specifies benfords law method test
-    # returns the p value of the test
-
-    # Initialize
-    bl = benfordslaw(alpha=0.05, method=method)
-    # data expected as a 1D list of values
-    X = pd.DataFrame({'values':data})
-    testData = X['values']
-    results = bl.fit(testData)
-    bl.plot()
-    return results['P']
-
-
 def calculateMagnitude(inputList):
     minVal = min(inputList)
     minOrder = len(str(minVal))-1
@@ -65,59 +46,62 @@ def calculateMagnitude(inputList):
            
 
 def calulateWeightings(l, magnitudeOrder):
-    return (magnitudeOrder**l)/(6**l)
+    return ((magnitudeOrder**l)/(6**l)+0.1)
 
 
 def testProcedure(importedData, method, l):
     # imported data will have three sets of data - likes, follows and tweets
     # the p values of each data set will be weighted averaged
-    followers = importedData['followers']
     likes = importedData['likes']
     retweets = importedData['retweets']
 
-    #pFollowers = benfordsLawTest(followers, method=method)
-    pFollowers = 1
-    pLikes = benfordsLawTest(likes, method=method)
-    pRetweets = benfordsLawTest(retweets, method=method)
+    # frequency analysis of leading digit data set
+    observedRetweets = frequencyCalc(retweets)
+    observedLikes = frequencyCalc(likes)
     
-    wFollowers = calulateWeightings(l, calculateMagnitude(followers))
+
+    # this returns 2 values, statistic and p value.
+    # it is assumed that the the two distributions are identical (null hypothesis)
+    # if stat is small or P  value is large, we accept the null hypothesis
+    # if stat is large or p small, we reject the null hypothesis and say that they are different distribtuions
+    # i.e. the data set does not follow benfords law.
+    retweetStat, retweetP = stats.ks_2samp(observedRetweets, expected)
+    likesStat, likesP = stats.ks_2samp(observedLikes, expected)
+    
+    # calculate weightings
     wLikes = calulateWeightings(l, calculateMagnitude(likes))
     wRetweets = calulateWeightings(l, calculateMagnitude(retweets))
 
-    print(wFollowers, wLikes, wRetweets)
+    print(wLikes, wRetweets)
 
-    pAverage = ((pFollowers * wFollowers) + (pLikes * wLikes) + (pRetweets * wRetweets))/(wFollowers + wLikes + wRetweets)
+    pAverage = ((likesStat * wLikes) + (retweetStat * wRetweets))/(wLikes + wRetweets)
     print("P Average:\t", pAverage)
-    return pAverage, (wFollowers+wLikes+wRetweets)/3
-
-    
-
+    return pAverage, (wLikes+wRetweets)/2
     
 
 
-data = {'followers' :[1929345],
-        'likes' :[],
+
+def test(accountName):
+    data = {'likes' :[],
         'retweets': []}
 
- 
+    global numbers
+    numbers = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0,6: 0, 7: 0, 8: 0, 9: 0}
 
-# get data
-returnedData = get_account_info("earthquakesSF", 1000)
+    # get data
+    returnedData = get_account_info(accountName, 1000)
+
+    data['likes'] = returnedData.tweet_likes
+    data['retweets'] = returnedData.tweet_retweets
+    print(data)
+    finalP, confidence = testProcedure(data, 'ks', L)
+    if finalP > 0.35:
+        return True, finalP, confidence
+    else:
+        return False, finalP, confidence
 
 
-data['likes'] = returnedData.tweet_likes
-data['retweets'] = returnedData.tweet_retweets
-print(data)
-print(testProcedure(data, 'ks', L))
-
-observed = frequencyCalc(data['retweets'])
-# observed = [10.1, 17.6, 7.5, 16.7, 9.9, 1.7, 8.8, 10.1, 11.6]
-expected = [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
-print(observed)
-
-# this returns 2 values, statistic and p value.
-# it is assumed that the the two distributions are identical (null hypothesis)
-# if stat is small or P  value is large, we accept the null hypothesis
-# if stat is large or p small, we reject the null hypothesis and say that they are different distribtuions
-# i.e. the data set does not follow benfords law.
-print(stats.ks_2samp(observed, expected))
+accounts = ["imperialCollege", "BBC", "A12_Info", "HEISEI_love_bot", "earthquakesSF"]
+for account in accounts:
+    bot, p, confidence = test(account)
+    print(account, ":", bot)
