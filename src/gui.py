34518ec9874845a3a91ad449932ab6b31ff1dd26
William West <ww1321@ic.ac.uk>
2022-02-06 09:52:37 +0000

    Added max bot chance, added positivity and colours

--- src/gui.py
+++ src/gui.py
@@ -11,14 +11,15 @@ SIZE = 36
 class Twitter(QWidget):
     def __init__(self):
         super().__init__()
-        f = QtGui.QFont()
-        f.setPixelSize(SIZE)
+        self.f = QtGui.QFont()
+        self.f.setPixelSize(SIZE)
 
         label = QLabel("Enter an account to be analysed:", self)
         label.setAlignment(QtCore.Qt.AlignCenter)
         self.bot_chance = QLabel(self)
         self.fake_news_chance = QLabel(self)
         self.positivity = QLabel(self)
+        self.label2 = QLabel(self)
         self.input = QLineEdit(self)
         self.submit = QPushButton("Analyse", self)
         self.loading = QLabel(self)
@@ -40,22 +41,50 @@ class Twitter(QWidget):
         hlayout = QHBoxLayout()
         hlayout.addWidget(self.input)
         hlayout.addWidget(self.submit)
+        hlayout2 = QHBoxLayout()
+        hlayout2.setAlignment(QtCore.Qt.AlignLeft)
+        hlayout2.addWidget(self.label2)
+        hlayout2.addWidget(self.positivity)
+
         self.vlayout.addWidget(label)
         self.vlayout.addLayout(hlayout)
         self.vlayout.addWidget(self.bot_chance)
         self.vlayout.addWidget(self.fake_news_chance)
-        self.vlayout.addWidget(self.positivity)
-
-        self.setFont(f)
+        self.vlayout.addLayout(hlayout2)
+        self.label2.hide()
+        self.positivity.hide()
+        self.setFont(self.f)
         self.resize(1200, 500)
+        self.label2.setText("Positivity Rating:")
         self.loading.move(self.width()//2 - 75, int(self.height()*2/3) - 60)
         self.show()
 
     def get_probabilities(self, search):
         from random import randint
         try:
+            font_style = "font-size: 36px"
             sentiment, bot = accountBotTest(search)
+            bot = min(bot, 99)
             self.bot_chance.setText("Bot Probability: " + "{:.1f}".format(bot) + "%")
+            self.positivity.setText(sentiment)
+            if bot < 50:
+                self.bot_chance.setStyleSheet(f"color: rgb({bot/50*255}, 255, 0); {font_style}")
+            else:
+                self.bot_chance.setStyleSheet(f"color: rgb(255, {(100-bot)/50*255}, 0); {font_style}")
+
+            font_style = "font-size: 36px"
+
+            if sentiment == "Very Negative":
+                self.positivity.setStyleSheet("color: rgb(255, 0, 0);" + font_style)
+            elif sentiment == "Slightly Negative":
+                self.positivity.setStyleSheet("color: rgb(255, 60, 0);" + font_style)
+            elif sentiment == "Slightly Positive":
+                self.positivity.setStyleSheet("color: rgb(111, 200, 0);" + font_style)
+            elif sentiment == "Very Positive":
+                self.positivity.setStyleSheet("color: rgb(0, 255, 0);" + font_style)
+            else:
+                self.positivity.setStyleSheet("color: rgb(255, 255, 0);" + font_style)
+
         except BaseException as e:
             print(e)
             self.bot_chance.setText("Error: User does not exist!")
@@ -66,11 +95,15 @@ class Twitter(QWidget):
         self.fake_news_chance.setText("Fake News Probability: " + str(randint(0, 100)))
         self.bot_chance.show()
         self.fake_news_chance.show()
+        self.label2.show()
+        self.positivity.show()
 
     def analyse(self):
         self.fake_news_chance.hide()
         self.bot_chance.hide()
+        self.label2.hide()
         self.loading.show()
+        self.positivity.hide()
         self.loading_gif.start()
         search = self.input.text()
         thread = threading.Thread(target=self.get_probabilities, args=[search])
